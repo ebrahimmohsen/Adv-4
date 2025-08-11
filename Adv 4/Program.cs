0336b245dab1375e19ebdb65ebb8730c74b65ca7
ebrahimmohsen <ebrahimzaki456@gmail.com>
2025-08-11 13:38:16 +0300

    Q1 solved

--- Adv 4/Program.cs
+++ Adv 4/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+        #region Q1 Count Frequency of Each Element
+        static void CountFrequencies(int[] arr)
+        {
+            var freq = new Dictionary<int, int>();
+            foreach (int num in arr)
+                if (freq.ContainsKey(num)) freq[num]++;
+                else freq[num] = 1;
+
+            foreach (var kv in freq)
+                Console.WriteLine($"{kv.Key} : {kv.Value}");
+        }
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 Count Frequency of Each Element
+            int[] arrQ1 = { 1, 2, 2, 3, 1, 4 };
+            CountFrequencies(arrQ1);
+            #endregion
         }
     }
 }
